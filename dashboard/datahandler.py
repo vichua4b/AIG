b83a591611fcc035acf9c3d068aad1f4d07ac2ba
vichua4b <vichua4b@gmail.com>
2025-04-06 18:24:34 +1000

    add royal clock

--- dashboard/datahandler.py
+++ dashboard/datahandler.py
@@ -44,7 +44,8 @@ INDUSTRY_GROUPS_OPTION = [
     # OECD-MEND_M_CI-PosNegRet
     'OECD-MEND_M_CI-PosNegRet-1-NO',
     'OECD-MEND_M_CI-PosNegRet-1-NO-2022',
-    'OECD-MEND_M_CI-PosNegRetNoStag-1-NO-2022'
+    'OECD-MEND_M_CI-PosNegRetNoStag-1-NO-2022',
+    'royal_clock'
 ]
 
 @st.cache_data
