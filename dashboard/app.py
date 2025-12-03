2301e6a2c35f7524be4173be9b05084d0e7a58bb
vichua4b <vichua4b@gmail.com>
2025-12-03 20:47:46 +1100

    add new page

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,6 +5,7 @@ import pandas as pd
 st.title('AIG dashboard')
 
 correlation = st.Page("pages/US/correlation.py", title="Correlation", icon=":material/analytics:", default=True)
+correlation_download = st.Page("pages/US/correlation_download.py", title="Correlation download", icon=":material/download:")
 
 compare_bt = st.Page("pages/HK/compare_backtest.py", title="Compare Backtest", icon=":material/finance_mode:")
 regime_bt = st.Page("pages/HK/regime_backtest.py", title="Regime Backtest", icon=":material/chart_data:")
@@ -14,7 +15,7 @@ universe_stats = st.Page("pages/HK/universe_stats.py", title="Universe Stats", i
 
 pg = st.navigation(
     {
-        "US - 2025": [correlation],
+        "US - 2025": [correlation, correlation_download],
         "HK - royal clock": [universe_stats, sector_stats, regime_bt, compare_bt, clock],
     }
 )
